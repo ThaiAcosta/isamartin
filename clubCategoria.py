9043cf440bb9130a41c3e82a86c7c69082dbb5f2
ThaiAcosta <isaias.acosta@sagradoalcal.edu.ar>
2026-06-30 17:05:23 -0300

    agregamos metodos en clubCategoria

--- clubCategoria.py
+++ clubCategoria.py
@@ -1,14 +1,33 @@
 from club import Club
 
-class Clublosnoaceptados(Club):
-    def __init__(self, nombre, descripcion, ubicacion, presidente, fecha_fundacion, actividades):
+class Clublosavengers(Club):
+    def __init__(self, nombre, descripcion, ubicacion, presidente, fecha_fundacion):
         super().__init__(nombre, descripcion, ubicacion, presidente, fecha_fundacion)
         self.__socios = []
-        self.actividades = actividades
+        self.actividades = []
         
     def get_socios(self):
         return self.__socios
     
     def set_socios(self, __socios):
         self.__socios = __socios
+    
+    def registrar_socio(self, socio):
+        self.__socios.append(socio)
+    
+    def cantidad_socios(self):
+        return len(self.__socios)
+
+    def agregar_actividad(self, actividad):
+        self.actividades.append(actividad)
         
+    def mostrar_actividades(self):
+        print(self.actividades)
+        
+madrid = Clublosavengers("isa", "lugar sucio", "abajo del puente (casa de martin)", "marino", 2024)
+
+madrid.registrar_socio("marina")
+madrid.cantidad_socios()
+madrid.agregar_actividad("bailar la danza de la sabiduria ancestral y lluvia dietetica que le enseñaron los tios de martin a marino")
+madrid.agregar_actividad("comer como le enseño isa a gio el malageño uuaaa")
+madrid.mostrar_actividades()
