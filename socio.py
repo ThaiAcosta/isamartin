7502c84d565fecb17403cb0059e9ab971b459896
ThaiAcosta <isaias.acosta@sagradoalcal.edu.ar>
2026-07-06 16:12:16 -0300

    modifique el archivo socio agregando metodos get y set para los atributos privados

--- socio.py
+++ socio.py
@@ -1,6 +1,18 @@
 class Socio:
-    def __init__(self, fecha_incripcion, estado, usuario, contraseña )
+    def __init__(self, fecha_incripcion, estado, usuario, contrasenia ):
         self.fecha_incripcion = fecha_incripcion
         self.estado = estado
-        self.
+        self.__usuario = usuario
+        self.__contrasenia = contrasenia
     
+    def get__usuario(self):
+        return self.__usuario
+    
+    def set__usuario(self, __usuario_nuevo):
+        self.__usuario = __usuario_nuevo
+
+    def get__contrasenia(self):
+        return self.__contrasenia
+    
+    def set__contrasenia(self, __contrasenia_nueva):
+        self.__contrasenia = __contrasenia_nueva
